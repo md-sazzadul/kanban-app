925efbcf7167e644f9d2320eb63c2811deb5e7f3
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-31 10:27:10 +0200

    edit task

--- src/features/task/TaskCard.tsx
+++ src/features/task/TaskCard.tsx
@@ -1,7 +1,11 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
+import Modal from "../../components/Modal";
 import type { Task } from "../../types";
 import { cn } from "../../utils/cn";
+import TaskForm from "./TaskForm";
+import { useTaskStore } from "./taskStore";
 
 type Props = {
   task: Task;
@@ -14,6 +18,10 @@ const priorityStyles = {
 };
 
 const TaskCard = ({ task }: Props) => {
+  const [open, setOpen] = useState(false);
+  const updateTask = useTaskStore((s) => s.updateTask);
+  const deleteTask = useTaskStore((s) => s.deleteTask);
+
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({
       id: task.id,
@@ -25,24 +33,44 @@ const TaskCard = ({ task }: Props) => {
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      {...listeners}
-      {...attributes}
-      style={style}
-      className="bg-white dark:bg-gray-700 p-3 rounded shadow cursor-pointer"
-    >
-      <h3 className="font-medium">{task.title}</h3>
-
-      <span
-        className={cn(
-          "text-xs px-2 py-1 rounded mt-2 inline-block",
-          priorityStyles[task.priority],
-        )}
+    <>
+      <div
+        ref={setNodeRef}
+        {...listeners}
+        {...attributes}
+        style={style}
+        className="bg-white dark:bg-gray-700 p-3 rounded shadow cursor-pointer"
+        onClick={() => setOpen(true)}
       >
-        {task.priority}
-      </span>
-    </div>
+        <h3 className="font-medium">{task.title}</h3>
+
+        <span
+          className={cn(
+            "text-xs px-2 py-1 rounded mt-2 inline-block",
+            priorityStyles[task.priority],
+          )}
+        >
+          {task.priority}
+        </span>
+      </div>
+
+      <Modal isOpen={open} onClose={() => setOpen(false)}>
+        <TaskForm
+          defaultValues={task}
+          onSubmit={(data) => {
+            updateTask({ ...task, ...data });
+            setOpen(false);
+          }}
+        />
+
+        <button
+          onClick={() => deleteTask(task.id)}
+          className="mt-3 text-red-500"
+        >
+          Delete Task
+        </button>
+      </Modal>
+    </>
   );
 };
 
