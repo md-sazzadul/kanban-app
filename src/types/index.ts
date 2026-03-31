cef3e3090963675a16f31973b53b4efabdf7c8f0
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-31 08:38:42 +0200

    define types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,20 @@
 export type ID = string;
+
+export type Board = {
+  id: string;
+  title: string;
+};
+
+export type Column = {
+  id: string;
+  title: string;
+  boardId: string;
+};
+
+export type Task = {
+  id: string;
+  title: string;
+  priority: "low" | "medium" | "high";
+  columnId: string;
+  boardId: string;
+};
