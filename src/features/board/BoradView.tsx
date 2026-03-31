242341f91d5670f75cd810910721c800b810f4f6
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-31 09:55:16 +0200

    wrap board with dnd context

--- src/features/board/BoradView.tsx
+++ src/features/board/BoradView.tsx
@@ -1,24 +1,40 @@
+import { closestCorners, DndContext, type DragEndEvent } from "@dnd-kit/core";
 import Column from "../column/Column";
 import { useColumnStore } from "../column/columnStore";
+import { useTaskStore } from "../task/taskStore";
 import { useBoardStore } from "./boardStore";
 
 const BoardView = () => {
   const activeBoardId = useBoardStore((s) => s.activeBoardId);
   const columns = useColumnStore((s) => s.columns);
   const loading = useBoardStore((s) => s.loading);
+  const moveTask = useTaskStore((s) => s.moveTask);
 
   const filteredColumns = columns.filter(
     (col) => col.boardId === activeBoardId,
   );
 
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const taskId = active.id as string;
+    const targetColumnId = over.id as string;
+
+    moveTask(taskId, targetColumnId);
+  };
+
   if (loading) return <div className="p-10">Loading...</div>;
 
   return (
-    <div className="flex gap-4 p-6 overflow-x-auto h-full">
-      {filteredColumns.map((col) => (
-        <Column key={col.id} column={col} />
-      ))}
-    </div>
+    <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+      <div className="flex gap-4 p-6 overflow-x-auto h-full">
+        {filteredColumns.map((col) => (
+          <Column key={col.id} column={col} />
+        ))}
+      </div>
+    </DndContext>
   );
 };
 
