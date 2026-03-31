f62aaaafe6c5c7384c5ebaa7d266f19c351d3e07
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-31 09:27:08 +0200

    create board view

---
+++ src/features/board/BoradView.tsx
@@ -0,0 +1,21 @@
+import { useColumnStore } from "../column/columnStore";
+import { useBoardStore } from "./boardStore";
+
+const BoardView = () => {
+  const activeBoardId = useBoardStore((s) => s.activeBoardId);
+  const columns = useColumnStore((s) => s.columns);
+
+  const filteredColumns = columns.filter(
+    (col) => col.boardId === activeBoardId,
+  );
+
+  return (
+    <div className="flex gap-4 p-6 overflow-x-auto h-full">
+      {filteredColumns.map((col) => (
+        <Column key={col.id} column={col} />
+      ))}
+    </div>
+  );
+};
+
+export default BoardView;
