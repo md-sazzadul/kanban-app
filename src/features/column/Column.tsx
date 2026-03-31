3471c865fec59894e2134315399add5ea151438d
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-31 09:56:41 +0200

    make column droppable

--- src/features/column/Column.tsx
+++ src/features/column/Column.tsx
@@ -1,3 +1,4 @@
+import { useDroppable } from "@dnd-kit/core";
 import type { Column as ColumnType } from "../../types";
 import TaskCard from "../task/TaskCard";
 import { useTaskStore } from "../task/taskStore";
@@ -9,10 +10,17 @@ type Props = {
 const Column = ({ column }: Props) => {
   const tasks = useTaskStore((s) => s.tasks);
 
+  const { setNodeRef } = useDroppable({
+    id: column.id,
+  });
+
   const columnTasks = tasks.filter((task) => task.columnId === column.id);
 
   return (
-    <div className="bg-gray-100 dark:bg-gray-800 rounded p-4 w-72 min-w-70">
+    <div
+      ref={setNodeRef}
+      className="bg-gray-100 dark:bg-gray-800 rounded p-4 w-72 min-w-70"
+    >
       <h2 className="font-semibold mb-4">{column.title}</h2>
 
       <div className="flex flex-col gap-2">
