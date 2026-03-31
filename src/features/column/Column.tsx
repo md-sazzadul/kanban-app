e7ddbd3b2b0e18aa1ff2fe22660b9a00a85ead26
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-31 09:38:11 +0200

    create column component

---
+++ src/features/column/Column.tsx
@@ -0,0 +1,26 @@
+import type { Column as ColumnType } from "../../types";
+import { useTaskStore } from "../task/taskStore";
+
+type Props = {
+  column: ColumnType;
+};
+
+const Column = ({ column }: Props) => {
+  const tasks = useTaskStore((s) => s.tasks);
+
+  const columnTasks = tasks.filter((task) => task.columnId === column.id);
+
+  return (
+    <div className="bg-gray-100 dark:bg-gray-800 rounded p-4 w-72 min-w-70">
+      <h2 className="font-semibold mb-4">{column.title}</h2>
+
+      <div className="flex flex-col gap-2">
+        {columnTasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Column;
